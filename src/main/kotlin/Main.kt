77fe01518febd6df6ac211cc78ef5e538db9a9be
SzymonSawczuk <szymon15380@gmail.com>
2025-01-06 10:35:44 +0100

    Felina

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,8 +9,11 @@ import pl.edu.pw.solution.ThreadsStructuralSolution
 fun main() {
 
     val configs = listOf(
-        ConfigurationProvider(100, 1e-12, 1, 2),
-        ConfigurationProvider(100, 1e-12, 1, 3)
+        ConfigurationProvider(2950, 1e-12, 3, 2),
+        ConfigurationProvider(2950, 1e-12, 3, 4),
+        ConfigurationProvider(2950, 1e-12, 3, 6),
+        ConfigurationProvider(2950, 1e-12, 3, 8),
+        ConfigurationProvider(2950, 1e-12, 3, 10),
     )
     configs.forEach { config ->
         println("\nConfiguration: $config")
